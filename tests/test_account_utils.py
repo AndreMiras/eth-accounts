5fcb82e6ad27ef9b6937f352109d2671ef313d74
Andre Miras <andre.miras@gmail.com>
2019-12-25 22:52:17 +0100

    TestAccountUtils via pytest

--- tests/test_account_utils.py
+++ tests/test_account_utils.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-import unittest
+import pytest
 from tempfile import TemporaryDirectory, mkdtemp
 from unittest import mock
 
@@ -10,13 +10,13 @@ from eth_accounts.account import Account
 PASSWORD = "password"
 
 
-class TestAccountUtils(unittest.TestCase):
+class TestAccountUtils:
 
-    def setUp(self):
+    def setup_method(self):
         self.keystore_dir = mkdtemp()
         self.account_utils = AccountUtils(self.keystore_dir)
 
-    def tearDown(self):
+    def teardown_method(self):
         shutil.rmtree(self.keystore_dir, ignore_errors=True)
 
     def test_new_account(self):
@@ -27,36 +27,36 @@ class TestAccountUtils(unittest.TestCase):
         3) tries to unlock the account
         """
         # 1) verifies the current account list is empty
-        self.assertEqual(self.account_utils.get_account_list(), [])
+        assert self.account_utils.get_account_list() == []
         # 2) creates a new account and verify we can retrieve it
         password = PASSWORD
         account = self.account_utils.new_account(password, iterations=1)
-        self.assertEqual(len(self.account_utils.get_account_list()), 1)
-        self.assertEqual(account, self.account_utils.get_account_list()[0])
+        assert len(self.account_utils.get_account_list()) == 1
+        assert account == self.account_utils.get_account_list()[0]
         # 3) tries to unlock the account
         # it's unlocked by default after creation
-        self.assertFalse(account.locked)
+        assert account.locked is False
         # let's lock it and unlock it back
         account.lock()
-        self.assertTrue(account.locked)
+        assert account.locked is True
         account.unlock(password)
-        self.assertFalse(account.locked)
+        assert account.locked is False
 
     def test_get_account_list(self):
         """
         Makes sure get_account_list() loads properly accounts from file system.
         """
         password = PASSWORD
-        self.assertEqual(self.account_utils.get_account_list(), [])
+        assert self.account_utils.get_account_list() == []
         account = self.account_utils.new_account(password, iterations=1)
-        self.assertEqual(len(self.account_utils.get_account_list()), 1)
+        assert len(self.account_utils.get_account_list()) == 1
         account = self.account_utils.get_account_list()[0]
-        self.assertIsNotNone(account.path)
+        assert account.path is not None
         # removes the cache copy and checks again if it gets loaded
         self.account_utils._accounts = None
-        self.assertEqual(len(self.account_utils.get_account_list()), 1)
+        assert len(self.account_utils.get_account_list()) == 1
         account = self.account_utils.get_account_list()[0]
-        self.assertIsNotNone(account.path)
+        assert account.path is not None
 
     def test_get_account_list_error(self):
         """
@@ -69,15 +69,15 @@ class TestAccountUtils(unittest.TestCase):
         with open(account.path, 'w') as f:
             f.write(account.dump())
         # `listdir()` can raise a `PermissionError`
-        with mock.patch('os.listdir') as mock_listdir:
-            mock_listdir.side_effect = PermissionError
-            with self.assertRaises(PermissionError):
-                self.account_utils.get_account_list()
+        with mock.patch('os.listdir') as m_listdir, \
+                pytest.raises(PermissionError):
+            m_listdir.side_effect = PermissionError
+            self.account_utils.get_account_list()
         # the empty account list should not be catched and loading it again
         # should show the existing account on file system
-        self.assertEqual(len(self.account_utils.get_account_list()), 1)
-        self.assertEqual(
-            self.account_utils.get_account_list()[0].address, account.address)
+        assert len(self.account_utils.get_account_list()) == 1
+        assert (
+            self.account_utils.get_account_list()[0].address == account.address)
 
     def test_get_account_list_no_dir(self):
         """
@@ -86,14 +86,14 @@ class TestAccountUtils(unittest.TestCase):
         """
         # nominal case when the directory already exists
         with TemporaryDirectory() as keystore_dir:
-            self.assertTrue(os.path.isdir(keystore_dir))
+            assert os.path.isdir(keystore_dir) is True
             account_utils = AccountUtils(keystore_dir)
-            self.assertEqual(account_utils.get_account_list(), [])
+            assert account_utils.get_account_list() == []
         # when the directory doesn't exist it should also be created
-        self.assertFalse(os.path.isdir(keystore_dir))
+        assert os.path.isdir(keystore_dir) is False
         account_utils = AccountUtils(keystore_dir)
-        self.assertTrue(os.path.isdir(keystore_dir))
-        self.assertEqual(account_utils.get_account_list(), [])
+        assert os.path.isdir(keystore_dir) is True
+        assert account_utils.get_account_list() == []
         shutil.rmtree(keystore_dir, ignore_errors=True)
 
     def test_deleted_account_dir(self):
@@ -111,9 +111,8 @@ class TestAccountUtils(unittest.TestCase):
             '/tmp/keystore//',
         ]
         for keystore_dir in keystore_dirs:
-            self.assertEqual(
-                AccountUtils.deleted_account_dir(keystore_dir),
-                expected_deleted_keystore_dir)
+            assert AccountUtils.deleted_account_dir(
+                keystore_dir) == expected_deleted_keystore_dir
 
     def test_delete_account(self):
         """
@@ -123,20 +122,19 @@ class TestAccountUtils(unittest.TestCase):
         password = PASSWORD
         account = self.account_utils.new_account(password, iterations=1)
         address = account.address
-        self.assertEqual(len(self.account_utils.get_account_list()), 1)
+        assert len(self.account_utils.get_account_list()) == 1
         # deletes the account and verifies it's not loaded anymore
         self.account_utils.delete_account(account)
-        self.assertEqual(len(self.account_utils.get_account_list()), 0)
+        assert len(self.account_utils.get_account_list()) == 0
         # even recreating the AccountUtils object
         self.account_utils = AccountUtils(self.keystore_dir)
-        self.assertEqual(len(self.account_utils.get_account_list()), 0)
+        assert len(self.account_utils.get_account_list()) == 0
         # tries to reload it from the backup/trash location
         deleted_keystore_dir = AccountUtils.deleted_account_dir(
             self.keystore_dir)
         self.account_utils = AccountUtils(deleted_keystore_dir)
-        self.assertEqual(len(self.account_utils.get_account_list()), 1)
-        self.assertEqual(
-            self.account_utils.get_account_list()[0].address, address)
+        assert len(self.account_utils.get_account_list()) == 1
+        assert self.account_utils.get_account_list()[0].address == address
 
     def test_delete_account_already_exists(self):
         """
@@ -159,9 +157,9 @@ class TestAccountUtils(unittest.TestCase):
         # create that file
         open(deleted_account_path, 'a').close()
         # then deletes the account and verifies it worked
-        self.assertEqual(len(self.account_utils.get_account_list()), 1)
+        assert len(self.account_utils.get_account_list()) == 1
         self.account_utils.delete_account(account)
-        self.assertEqual(len(self.account_utils.get_account_list()), 0)
+        assert len(self.account_utils.get_account_list()) == 0
 
     def test_get_or_create(self):
         """
