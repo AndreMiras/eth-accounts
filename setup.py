6152c07ac94276832b8735618ab1561c81bfa8d1
Andre Miras <andre.miras@gmail.com>
2019-12-27 15:47:15 +0100

    20191227

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AndreMiras/eth_accounts',
-    version='20191226',
+    version='20191227',
     zip_safe=False,
 )
